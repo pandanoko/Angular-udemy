724c460345f07e62104578bc09697e1c913b1fa6
Sara Russo <pandessa@gmail.com>
2024-04-10 14:25:51 +0200

    submit assignemnt

--- basics-assignment-2/src/app/app.component.ts
+++ basics-assignment-2/src/app/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  userName = '';
+  displayedName= '';
+  //isButtonDisabled = this.userName.length > 0 ? false : true;
+
+  onSubmitName() {
+    this.displayedName = this.userName ;
+    this.userName = '';
+  }
 }
