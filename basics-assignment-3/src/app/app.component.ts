beb5413abcf6c9b2a14e0dec427dee8c2245d1ae
Sara Russo <pandessa@gmail.com>
2024-04-10 17:20:09 +0200

    solution assignment 3

--- basics-assignment-3/src/app/app.component.ts
+++ basics-assignment-3/src/app/app.component.ts
@@ -3,7 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+    .highlight {
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
+  paragraphIsVisible = false;
+  clickLogs = [];
+
+  toggleVisibility() {
+    this.paragraphIsVisible = !this.paragraphIsVisible;
+    //this.clickLogs.push(this.clickLogs.length + 1);
+    this.clickLogs.push(new Date());
+  }
 }
