820f4dae86f0ee79f28f6dc8f9ebeb01d8d7cfd8
Sara Russo <pandessa@gmail.com>
2024-04-11 15:52:44 +0200

    update components

--- recipe-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
-    selector: 'recipe-list',
-    templateUrl: 'recipe-list.component.html'
+  selector: 'recipe-list',
+  templateUrl: 'recipe-list.component.html',
 })
-export class RecipeListComponent {}
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'Test description',
+      'https://images.unsplash.com/photo-1466637574441-749b8f19452f?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    ),
+    new Recipe(
+        'Test Recipe',
+        'Test description',
+        'https://images.unsplash.com/photo-1466637574441-749b8f19452f?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      ),
+  ];
+}
