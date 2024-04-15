6bfe0f4e890d3f4179d8ce3744a6d0eca4e6569c
Sara Russo <pandessa@gmail.com>
2024-04-15 15:13:20 +0200

    passing data

--- recipe-app/src/app/recipe-book/recipe-book.component.ts
+++ recipe-app/src/app/recipe-book/recipe-book.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { Recipe } from './recipe.model'
 
 @Component({
     selector: 'recipe-book',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core'
 })
 
 export class RecipeBookComponent {
-
+    selectedRecipe: Recipe;
 }
