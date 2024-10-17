a677e3ef90f7085eea410a914f0b7edb189e3b9a
Sara Russo <pandessa@gmail.com>
2024-10-17 10:24:58 +0200

    added prettier configuration + chained subscription + loading state

--- recipe-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,41 +1,60 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs'
+import { Subscription } from 'rxjs';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../../shared/recipe.service';
 import { DataStorageService } from '../../shared/data-storage.service';
 
-
 @Component({
   selector: 'recipe-list',
   templateUrl: 'recipe-list.component.html',
 })
-export class RecipeListComponent implements OnInit, OnDestroy{
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
-  subscription: Subscription; 
+  isLoading: boolean = false;
+  private subscriptions: Subscription = new Subscription(); // Composite subscription
 
-  constructor(private recipeService: RecipeService,
-              private dataStorageService: DataStorageService,
-              private router: Router,
-              private route: ActivatedRoute) {}
+  constructor(
+    private recipeService: RecipeService,
+    private dataStorageService: DataStorageService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
-    this.dataStorageService.fetchRecipes().subscribe();
-      this.subscription = this.recipeService.recipesChanged
-        .subscribe(
-          (recipes: Recipe[]) => {
-            this.recipes = recipes;
-          }
-        )
-      this.recipes= this.recipeService.getRecipes();
+    this.isLoading = true;
+    // Automatically fetch recipes and subscribe to recipe changes
+    this.subscriptions.add(
+      this.dataStorageService.fetchRecipes().subscribe({
+        next: () => {
+          this.isLoading = false;
+          // Handle successful fetch here if needed
+        },
+        error: (error) => {
+          this.isLoading = false;
+          console.error('Error fetching recipes:', error);
+        },
+      }),
+    );
+
+    // Subscribe to recipe changes
+    this.subscriptions.add(
+      this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
+        this.recipes = recipes;
+      }),
+    );
+
+    // Get initial recipes (if any are already loaded)
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onNewRecipe() {
-    this.router.navigate(['new'], {relativeTo: this.route});
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 
   ngOnDestroy(): void {
-      this.subscription.unsubscribe();
+    // Unsubscribe from all subscriptions at once
+    this.subscriptions.unsubscribe();
   }
 }
